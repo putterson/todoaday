68d911674189cf18d77ceda8aaa736b52e4a65b1
Pat Goebel <putterson@gmail.com>
2020-04-21 21:57:02 -0400

    Make .env.edn optional in config

--- src/clj/todoaday/config.clj
+++ src/clj/todoaday/config.clj
@@ -2,13 +2,17 @@
   (:require
     [cprop.core :refer [load-config]]
     [cprop.source :as source]
-    [mount.core :refer [args defstate]]))
+    [mount.core :refer [args defstate]]
+    [clojure.tools.logging :as log]))
 
 (defstate env
-  :start
-  (load-config
-    :merge
-    [(args)
-     (source/from-system-props)
-     (source/from-file ".env.edn")
-     (source/from-env)]))
+          :start
+          (load-config
+            :merge
+            [(args)
+             (source/from-system-props)
+             (try (source/from-file ".env.edn")
+                  (catch Throwable t
+                    (log/warn t "couldn't find .env.edn")
+                    {}))
+             (source/from-env)]))
